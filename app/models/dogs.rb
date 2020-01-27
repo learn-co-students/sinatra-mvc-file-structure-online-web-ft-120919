87cc6852012e0eceb0da2ab93a7fee5efcd76f2c
github username <github email address>
2020-01-27 14:31:01 -0800

    Done.

---
+++ app/models/dogs.rb
@@ -0,0 +1,11 @@
+# Create a new file in the models directory to create a dog class. 
+# This class should have name, breed, and age attributes which can be 
+# set on initialization. You should be able to read and write to these 
+# attributes. This class should also keep track of each instance of dog 
+# created, as well as a class method all to return an array of those instances.
+
+
+
+class Dogs < ActiveRecord::Base
+
+end
